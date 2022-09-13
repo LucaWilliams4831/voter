8248833ca1062a1e15169d406fb36b5b9e41cefc
panda141035 <panda141035@gmail.com>
2022-09-13 18:16:38 +0300

    ignite vote module sample

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,12 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Data from '../views/Data.vue'
 import Portfolio from '../views/Portfolio.vue'
+import Voter from '../views/Voter.vue'
 
 const routerHistory = createWebHistory()
 const routes = [
   { path: '/', component: Portfolio },
   { path: '/portfolio', component: Portfolio },
   { path: '/data', component: Data }
+  { path: '/voter', component: Voter }
+  
 ]
 
 const router = createRouter({
